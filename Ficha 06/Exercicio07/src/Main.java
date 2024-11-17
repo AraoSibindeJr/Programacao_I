3f1e1806ad21704364aaaa9d42526f88e359bb04
AraoSibindeJr <araosibinde098junior@gmail.com>
2024-11-17 19:46:57 +0200

    feat: Adicionado for loop para preeencher o array

--- Ficha 06/Exercicio07/src/Main.java
+++ Ficha 06/Exercicio07/src/Main.java
@@ -1,6 +1,18 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println(" ==> Tamanho do array: ");
+        int t = sc.nextInt();
+        int []v= new int[t];
+        int p = t;
+        // For loop para preencher o array
+        for(int i =0; i < v.length; i++){
+            System.out.println(" ==> Preencha o array com " + p + " espacos: ");
+            v[i]=sc.nextInt();
+            p--;
+        }
+
 
     }
 
