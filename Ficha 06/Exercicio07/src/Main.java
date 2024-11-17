45b3d618db323acf676b17444360d9ce1ff334d7
AraoSibindeJr <araosibinde098junior@gmail.com>
2024-11-17 20:22:56 +0200

    feat: Melhorada a saida de dados

--- Ficha 06/Exercicio07/src/Main.java
+++ Ficha 06/Exercicio07/src/Main.java
@@ -6,25 +6,28 @@ public class Main {
         int t = sc.nextInt();
         int []v= new int[t];
         int p = t, repeticoes =0;
+        System.out.println(" =====><=====");
         // For loop para preencher o array
         for(int i =0; i < v.length; i++){
             System.out.println(" ==> Preencha o array com " + p + " espacos: ");
             v[i]=sc.nextInt();
             p--;
         }
+        System.out.println(" =====><=====" );
+        System.out.println(" =====><=====");
         // Aquisicao do numero repetido
         System.out.println(" ==> Insira o numero repetido: ");
         int x = sc.nextInt();
-        for(int i = 0; i < v.length; i++){
-            if(v[i]==x){
+        for (int j : v) {
+            if (j == x) {
                 repeticoes++;
             }
         }
         // Saida de informacoes
-        System.out.println(" ==> Numero repetido: " + x);
+        System.out.println(" ===> Resultados_Finais <=== ");
+        System.out.println(" ==> Numero selecionado: " + x);
         System.out.println(" ==> Numero de repeticoes: "+ repeticoes);
-
-
+        System.out.println();
+        System.out.println(" ==> Fim <== ");
     }
-
 }
